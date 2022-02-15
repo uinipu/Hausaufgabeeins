3d438726d6186cab877558c8b4985a184ae7eba8
uinipu <91496899+uinipu@users.noreply.github.com>
2022-02-15 22:04:44 +0200

    aufgabe 3 jetzt

---
+++ src/Aufgabe3.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Aufgabe3 {
+    int[] x;
+    int[] y;
+
+    public Aufgabe3(){
+        System.out.println("Wie viele Ziffern gibt es in der ersten Zahl");
+    }
+}
