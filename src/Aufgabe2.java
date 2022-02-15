cc58068a39a24b574960459943cdde1821998fc0
uinipu <91496899+uinipu@users.noreply.github.com>
2022-02-15 21:48:16 +0200

    aufgabe 2 fertig

--- src/Aufgabe2.java
+++ src/Aufgabe2.java
@@ -10,11 +10,47 @@ public class Aufgabe2 {
         int n=s.nextInt();
         this.n=n;
         int[] array=new int[10];
-        System.out.println("Welch sind die "+n+"Elemente?");
+        System.out.println("Welch sind die "+n+" Elemente?");
         for(int i=0;i<n;i++){
             array[i]=s.nextInt();
         }
         this.array=array;
+    }
+
+    public int eins(){
+        int max=this.array[0];
+        for(int i=0;i<this.n;i++) {
+            if(this.array[i]>max)
+                max=this.array[i];
+        }
+        return max;
+    }
+
+    public int zwei(){
+        int min=this.array[0];
+        for(int i=0;i<this.n;i++) {
+            if(this.array[i]<min)
+                min=this.array[i];
+        }
+        return min;
+    }
+
+    public int sum(){
+        int sum=0;
+        for(int i=0;i<this.n;i++) {
+            sum+=this.array[i];
+        }
+        return sum;
+    }
+
+
+
+    public int drei(){
+        return this.sum()-this.zwei();
+    }
+
+    public int vier(){
+        return this.sum()-this.eins();
     }
 
 
--- src/main.java
+++ src/main.java
@@ -1,9 +1,20 @@
 public class main {
     public static void main(String[] args){
-    Aufgabe1 bsp = new Aufgabe1();
-    //bsp.a1ub1();
-    //System.out.println(bsp.a1ub2());
-    //bsp.a1ub3();
-    // System.out.println(bsp.a1ub4());
+        /*Aufgabe1 bsp = new Aufgabe1();
+
+        //bsp.a1ub1();
+        //System.out.println(bsp.a1ub2());
+        //bsp.a1ub3();
+        //System.out.println(bsp.a1ub4());
+        */
+
+        /*
+        Aufgabe2 ex=new Aufgabe2();
+
+        //System.out.println(ex.eins());
+        //System.out.println(ex.zwei());
+        //System.out.println(ex.drei());
+        //System.out.println(ex.vier());
+        */
     }
 }
