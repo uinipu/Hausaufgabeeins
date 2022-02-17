9da5847001b0c7cf7dc4d3346d948cb01182599e
uinipu <91496899+uinipu@users.noreply.github.com>
2022-02-17 10:04:14 +0200

    aufgabe 3 fertig

--- src/Aufgabe3.java
+++ src/Aufgabe3.java
@@ -36,26 +36,24 @@ public class Aufgabe3 {
             y[i] = s.nextInt();
         }
 
-        int p=0;
-        int [] result=new int[11];
-        result[0]=-1;
-        for(int i=n;i>0;i--){
-            int a=x[i]+y[i]+p;
-            if(a>9){
-                p=a/10;
-                a=a%10;
-            }
-            else
-                p=0;
-            result[i]=a;
+        int p = 0;
+        int[] result = new int[11];
+        result[0] = -1;
+        for (int i = n; i > 0; i--) {
+            int a = x[i] + y[i] + p;
+            if (a > 9) {
+                p = a / 10;
+                a = a % 10;
+            } else
+                p = 0;
+            result[i] = a;
         }
-        if(p!=0){
-            result[0]=p;
-            for(int i=0;i<=n;i++)
+        if (p != 0) {
+            result[0] = p;
+            for (int i = 0; i <= n; i++)
                 System.out.print(result[i]);
-        }
-        else{
-            for(int i=1;i<=n;i++)
+        } else {
+            for (int i = 1; i <= n; i++)
                 System.out.print(result[i]);
         }
     }
@@ -77,25 +75,23 @@ public class Aufgabe3 {
             y[i] = s.nextInt();
         }
 
-        int p=0;
-        int [] result=new int[10];
-        for(int i=n-1;i>=0;i--){
+        int p = 0;
+        int[] result = new int[10];
+        for (int i = n - 1; i >= 0; i--) {
             //int a=x[i]-y[i];
-            if(x[i]-y[i]<0){
-                x[i-1]-=1;
-                int b = 10+x[i];
-                result[i]=b-y[i];
-            }
-            else
-                result[i]=x[i]-y[i];
+            if (x[i] - y[i] < 0) {
+                x[i - 1] -= 1;
+                int b = 10 + x[i];
+                result[i] = b - y[i];
+            } else
+                result[i] = x[i] - y[i];
         }
 
-        if(result[0]!=0){
-            for(int i=0;i<n;i++)
+        if (result[0] != 0) {
+            for (int i = 0; i < n; i++)
                 System.out.print(result[i]);
-        }
-        else{
-            for(int i=1;i<n;i++)
+        } else {
+            for (int i = 1; i < n; i++)
                 System.out.print(result[i]);
         }
     }
@@ -113,29 +109,53 @@ public class Aufgabe3 {
 
         int a;
         System.out.println("Die zweite Zahl ist:");
-        a=s.nextInt();
+        a = s.nextInt();
 
-        int p=0;
-        int [] result=new int[10];
-        for(int i=n-1;i>=0;i--){
-            if(x[i]*a+p>10){
-                result[i]=(x[i]*a+p)%10;
-                p=(x[i]*a+p)/10;
-            }
-            else{
-                result[i]=x[i]*a+p;
-                p=0;
+        int p = 0;
+        int[] result = new int[10];
+        for (int i = n - 1; i >= 0; i--) {
+            if (x[i] * a + p > 10) {
+                result[i] = (x[i] * a + p) % 10;
+                p = (x[i] * a + p) / 10;
+            } else {
+                result[i] = x[i] * a + p;
+                p = 0;
             }
 
         }
 
 
-        for(int i=0;i<n;i++)
+        for (int i = 0; i < n; i++)
             System.out.print(result[i]);
 
     }
 
-    public void patru(){
+    public void patru() {
+        System.out.println("Wie viele Ziffern hat die Zahl?");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
 
+        System.out.println("Die Ziffern der ersten Zahl sind:");
+        int[] x = new int[10];
+        for (int i = 0; i < n; i++) {
+            x[i] = s.nextInt();
+        }
+
+        int a;
+        System.out.println("Die zweite Zahl ist:");
+        a = s.nextInt();
+
+        int[] result = new int[10];
+        int p=0;
+
+        for (int i = 0; i < n; i++) {
+            result[i]=(p*10+x[i])/a;
+            p=x[i]%a;
+        }
+
+
+       for (int i = 0; i < n; i++) {
+            System.out.print(result[i]);
+        }
     }
 }
